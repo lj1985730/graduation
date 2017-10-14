a1862c49bb4eb4f9d2ac4cf1e688f0848ef0442e
Liu Jun <lj1985730@gmail.com>
2017-10-14 18:20:58 +0800

    恢复错误修改

--- graduation-web/src/main/webapp/WEB-INF/scripts/global/metronic.js
+++ graduation-web/src/main/webapp/WEB-INF/scripts/global/metronic.js
@@ -355,18 +355,18 @@ var Metronic = function() {
 
     // Handles Bootstrap Modals.
     var handleModals = function() {        
-        // fix stackable model issue: when 2 or more modals opened, closing one of model will remove .model-open class.
+        // fix stackable modal issue: when 2 or more modals opened, closing one of modal will remove .modal-open class.
         $('body').on('hide.bs.modal', function() {
-            if ($('.model:visible').size() > 1 && $('html').hasClass('modal-open') === false) {
+            if ($('.modal:visible').size() > 1 && $('html').hasClass('modal-open') === false) {
                 $('html').addClass('modal-open');
-            } else if ($('.model:visible').size() <= 1) {
+            } else if ($('.modal:visible').size() <= 1) {
                 $('html').removeClass('modal-open');
             }
         });
 
         // fix page scrollbars issue
         $('body').on('show.bs.modal', '.modal', function() {
-            if ($(this).hasClass("model-scroll")) {
+            if ($(this).hasClass("modal-scroll")) {
                 $('body').addClass("modal-open-noscroll");
             }
         });
@@ -376,8 +376,8 @@ var Metronic = function() {
             $('body').removeClass("modal-open-noscroll");
         });
 
-        // remove ajax content and remove cache on model closed
-        $('body').on('hidden.bs.modal', '.model:not(.model-cached)', function () {
+        // remove ajax content and remove cache on modal closed
+        $('body').on('hidden.bs.modal', '.modal:not(.modal-cached)', function () {
             $(this).removeData('bs.modal');
         });
     };
